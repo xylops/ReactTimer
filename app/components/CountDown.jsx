f712b432aea23250c35a3a73e40571834d8b1e60
xylops <rankine1607@gmail.com>
2016-11-10 17:59:06 +0800

    update status to stop when count reaches zero

--- app/components/CountDown.jsx
+++ app/components/CountDown.jsx
@@ -10,6 +10,29 @@ var CountDown = React.createClass({
             countdownStatus:'stopped',
         };
     },
+    startTimer:function(){
+        this.timer = setInterval(() =>{
+            var newCount = this.state.count - 1;
+            this.setState({
+                count : newCount >= 0 ? newCount : 0
+            });
+
+            if(newCount === 0){
+                this.setState({countdownStatus:'stopped'});
+            }
+        }, 1000);
+    },
+    handleSetCountdown:function(seconds){
+        this.setState({
+            count: seconds,
+            countdownStatus:'started'
+        })
+    },
+    handleStatusChange:function(newStatus){
+        this.setState({
+            countdownStatus:newStatus
+        });
+    },
     componentDidUpdate:function(prevProps, prevState){
         if(this.state.countdownStatus !== prevState.countdownStatus){
             switch (this.state.countdownStatus){
@@ -25,24 +48,9 @@ var CountDown = React.createClass({
             }
         }
     },
-    startTimer:function(){
-        this.timer = setInterval(() =>{
-            var newCount = this.state.count - 1;
-            this.setState({
-                count : newCount >= 0 ? newCount : 0
-            });
-        }, 1000);
-    },
-    handleSetCountdown:function(seconds){
-        this.setState({
-            count: seconds,
-            countdownStatus:'started'
-        })
-    },
-    handleStatusChange:function(newStatus){
-        this.setState({
-            countdownStatus:newStatus
-        });
+    componentWillUnmount:function(){
+        clearInterval('this.timer')
+        this.timer=undefined;
     },
     render:function(){
         var {count, countdownStatus} = this.state;
