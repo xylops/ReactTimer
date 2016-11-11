9aeab6dabc438f23d6605fd65e1ecdbd1ad21622
xylops <rankine1607@gmail.com>
2016-11-11 12:46:21 +0800

    adding Timer app and test

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,9 +1,57 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
+    getInitialState:function(){
+        return ({
+            count: 0,
+            timerStatus:'paused',
+        })
+    },
+    startTimer:function(){
+        this.timer = setInterval(()=>{
+            var newCount = this.state.count + 1;
+            this.setState({
+                count : newCount,
+            })
+        },1000)
+    },
+    handleStatusChange : function(newState){
+        this.setState({
+            timerStatus : newState,
+        })
+    },
+    componentDidUpdate : function(prevProps, prevState){
+        if(this.state.timerStatus !== prevState.timerStatus){
+            switch (this.state.timerStatus){
+                case 'started':
+                    this.startTimer();
+                    break;
+                case 'stopped':
+                    this.setState({
+                        count:0,
+                        timerStatus:'paused'
+                    })
+                case 'paused':
+                    clearInterval(this.timer);
+                    this.timer = undefined;
+                    break;
+            }
+        }
+    },
+    componentWillUnmount:function(){
+        clearInterval(this.timer);
+    },
     render:function(){
+        var {count,timerStatus} = this.state;
+
         return(
-            <h2>Timer Component</h2>
+            <div>
+                <h1 className="page-title">Timer App</h1>
+                <Clock totalSeconds={count}/>
+                <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
+            </div>
         )
     },
 });
